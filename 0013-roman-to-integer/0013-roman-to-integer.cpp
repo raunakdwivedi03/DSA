cc9ea4cea5df252f12333ab6c9ea4b77c141029c
raunakdwivedi03 <147158247+raunakdwivedi03@users.noreply.github.com>
2025-08-19 12:21:22 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.34%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int num(char c){
+        if(c=='I') return 1;
+        else if(c=='V') return 5;
+        else if(c=='X') return 10;
+        else if(c=='L') return 50;
+        else if(c=='C') return 100;
+        else if(c=='D') return 500;
+        else if(c=='M') return 1000;
+
+        return 0;
+    };
+    int romanToInt(string s) {
+        int sum=0;
+        int i=0;
+        while(i<int(s.size())-1){
+        if(num(s[i])<num(s[i+1])){
+            sum-=num(s[i]);
+        }   
+        else{
+            sum+=num(s[i]);
+        }
+        i++;
+        }
+        sum+=num(s[s.size()-1]);
+        return sum;
+    }
+};
